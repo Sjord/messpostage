80421b06ff26ad310d77de00e6c9105b64925c7f
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2018-03-22 14:43:23 +0100

    Show rendered messages and listeners

--- messpostage/background.js
+++ messpostage/background.js
@@ -4,19 +4,30 @@
     var unread = 0;
 
     function notifyMessages() {
-        chrome.browserAction.setIcon({
-            path: "active_message_48.png"
-        });
-        chrome.browserAction.setTitle({
-            title: "You have unread messages"
-        });
-        chrome.browserAction.setBadgeText({
-            text: unread.toString()
-        });
+        if (unread) {
+            chrome.browserAction.setIcon({
+                path: "active_message_48.png"
+            });
+            chrome.browserAction.setTitle({
+                title: "You have unread messages"
+            });
+            chrome.browserAction.setBadgeText({
+                text: unread.toString()
+            });
+        } else {
+            chrome.browserAction.setIcon({
+                path: "no_message_48.png"
+            });
+            chrome.browserAction.setTitle({
+                title: "MessPostage"
+            });
+            chrome.browserAction.setBadgeText({
+                text: ""
+            });
+        }
     }
 
     chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-        console.log("recevied message ", request);
         if (request.type == "message") {
             messages.push(request.message);
             unread += 1;
@@ -24,8 +35,13 @@
         }
         if (request.type == "listener") {
             listeners.push(request.listener);
+            unread += 1;
+            notifyMessages();
         }
         if (request.type == "requestMessages") {
+            unread = 0;
+            notifyMessages();
+
             sendResponse({
                 "messages": messages,
                 "listeners": listeners
--- messpostage/popup.js
+++ messpostage/popup.js
@@ -1,9 +1,30 @@
 (function () {
-    chrome.runtime.sendMessage({type: "requestMessages"}, function(response) {
-        let container = document.getElementById("messages");
-        container.innerText = JSON.stringify(response.messages);
+    function trim_stack(stack) {
+        var lines = stack.split('\n');
+        lines = lines.slice(1);
+        return lines.join('\n');
+    }
 
-        let container2 = document.getElementById("listeners");
-        container2.innerText = JSON.stringify(response.listeners);
+    function render_listeners(target, listeners) {
+        for (let i in listeners) {
+            let listener = listeners[i];
+            let listenerDiv = document.createElement("div");
+            listenerDiv.innerText = trim_stack(listener.stack);
+            target.appendChild(listenerDiv);
+        }
+    }
+
+    function render_messages(target, messages) {
+        for (let i in messages) {
+            let message = messages[i];
+            let messageDiv = document.createElement("div");
+            messageDiv.innerText = JSON.stringify(message);
+            target.appendChild(messageDiv);
+        }
+    }
+
+    chrome.runtime.sendMessage({type: "requestMessages"}, function(response) {
+        render_messages(document.getElementById("messages"), response.messages);
+        render_listeners(document.getElementById("listeners"), response.listeners);
     });
 })();
