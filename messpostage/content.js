e4305744bfb7acfdb6a66242f1fba3b2b72d8b7f
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2018-07-31 15:40:58 +0200

    Only handle events with correct securityToken

--- messpostage/content.js
+++ messpostage/content.js
@@ -1,6 +1,8 @@
 (function () {
     "use strict";
 
+    let securityToken = JSON.stringify(crypto.getRandomValues(new Uint16Array(10)));
+
     /* Pass messages sent to this frame to the background script */
     window.addEventListener("message", function (message) {
         chrome.runtime.sendMessage({
@@ -13,19 +15,22 @@
     });
 
     window.addEventListener("messageListenerDetected", function (evt) {
-        chrome.runtime.sendMessage({
-            "type": "listener",
-            "listener": evt.detail
-        });
+        if (evt.detail.securityToken === securityToken) {
+            chrome.runtime.sendMessage({
+                "type": "listener",
+                "listener": evt.detail
+            });
+        }
     });
 
     /* Add a override.js to the page to override addEventListener */
-    function overrideFunctions() {
+    function overrideFunctions(securityToken) {
         let addEventListenerOrig = window.addEventListener;
         window.addEventListener = function(type, listener, useCapture, wantsUntrusted) {
             if (type == "message") {
                 let evt = new CustomEvent("messageListenerDetected", {detail: {
-                    stack: new Error().stack
+                    stack: new Error().stack,
+                    securityToken: securityToken
                 }});
                 window.dispatchEvent(evt);
             }
@@ -34,7 +39,7 @@
     };
 
     var s = document.createElement('script');
-    s.innerText = '(' + overrideFunctions + ')();'
+    s.innerText = '(' + overrideFunctions + ')(' + JSON.stringify(securityToken) + ');'
     s.onload = function() {
         this.remove();
     };
