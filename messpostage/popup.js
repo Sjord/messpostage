4323383ec5d49a907c8854906b56c90e626a0db3
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2018-08-21 16:04:10 +0200

    Use Mitrhil to render popup page

--- messpostage/popup.js
+++ messpostage/popup.js
@@ -1,45 +1,45 @@
 (function () {
     "use strict";
 
+    // Remove the first line of a stack trace
     function trimStack(stack) {
         let lines = stack.split("\n");
         lines = lines.slice(1);
         return lines.join("\n");
     }
 
-    function renderListeners(target, listeners) {
-        target.innerHTML  = '';
-        for (let i in listeners) {
-            let listener = listeners[i];
-            let listenerDiv = document.createElement("div");
-            listenerDiv.innerText = trimStack(listener.stack);
-            target.appendChild(listenerDiv);
+    // Component that calls stringifier on each data element.
+    class ResultList {
+        constructor(data, stringifier) {
+            this.data = data;
+            this.stringifier = stringifier;
+        }
+
+        view() {
+            return this.data.map(msg => m("div", this.stringifier(msg)));
         }
     }
 
-    function renderMessages(target, messages) {
-        target.innerHTML  = '';
-        for (let i in messages) {
-            let message = messages[i];
-            let messageDiv = document.createElement("div");
-            messageDiv.innerText = JSON.stringify(message);
-            target.appendChild(messageDiv);
-        }
-    }
-
-    document.addEventListener('DOMContentLoaded', function() {
-        var link = document.getElementById('clearButton');
-        // onClick's logic below:
-        link.addEventListener('click', function() {
-            chrome.runtime.sendMessage({type: "clearMessages"}, function(response) {
-                renderMessages(document.getElementById("messages"), response.messages);
-                renderListeners(document.getElementById("listeners"), response.listeners);
-            });
+    // Ask to clear the message list and rerender.
+    function clearMessages() {
+        chrome.runtime.sendMessage({type: "clearMessages"}, function(response) {
+            displayResponse(response);
         });
-    });
+    }
 
+    // Render the data response we got from the background page.
+    function displayResponse(response) {
+        m.render(document.body, [
+            m("h1", "messages"),
+            m("button", {onclick: clearMessages}, "clear"),
+            m(new ResultList(response.messages, JSON.stringify)),
+            m("h1", "listeners"),
+            m(new ResultList(response.listeners, l => trimStack(l.stack)))
+        ]);
+    }
+
+    // Request a new dataset from the background page.
     chrome.runtime.sendMessage({type: "requestMessages"}, function(response) {
-        renderMessages(document.getElementById("messages"), response.messages);
-        renderListeners(document.getElementById("listeners"), response.listeners);
+        displayResponse(response);
     });
 })();
